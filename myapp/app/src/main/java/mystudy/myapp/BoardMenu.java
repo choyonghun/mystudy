a76e035980b877340acda534e035674d83238fc7
choyonghun <43197549+choyonghun@users.noreply.github.com>
2023-12-06 19:52:56 +0900

    123

--- myapp/app/src/main/java/mystudy/myapp/BoardMenu.java
+++ myapp/app/src/main/java/mystudy/myapp/BoardMenu.java
@@ -3,8 +3,11 @@ package mystudy.myapp;
 public class BoardMenu {
 
   //static Board board = new Board(); 를 사용하면 여러개를 담을수없다!!
+  //new Board[2] 이면 0, 1 2개만 담는다는 뜻이다.
   static Board[] boards = new Board[2];
-  static int length = 0;  // static int length; 라고해도 초기화 된다!!
+
+  static int length = 0;
+  // static int length; 라고해도 초기화 된다!!
 
 
   static void printMenu() {
@@ -55,12 +58,14 @@ public class BoardMenu {
 
     if (length == boards.length) {    // 배열이 꽉찼다면
       System.out.println("게시글을 더이상 등록할수 없습니다.");
-      //return으로 보내도 되지만 배열을 1.5개씩 증가시켜준다.
+      //return으로 보내거나, 배열을 1.5개씩 증가시켜준다.
       int oldSize = boards.length;
       int newSize = oldSize + (oldSize / 2);
       //int newSize = oldSize + (oldSize >> 1);   1비트 이동시킨다. 2로 나눈다
 
-      //old를 지우고 new를 넣는다.
+      //새로운 크기의 newSize 배열을 만든다
+      //boards 안에있는 요소를 새로운 배열 arr[]에 그대로 집어넣는다.
+      //boards = arr 는 그대로 복사가 되어 확장된 배열에 참조된다.
       Board[] arr = new Board[newSize];
       for (int i = 0; i < oldSize; i++) {
         arr[i] = boards[i];
@@ -135,11 +140,15 @@ public class BoardMenu {
     }
 
     //0있는 값을 없애고 1에있는 값을 0로 가져온다
+    //index 값을 넣어줄때 [5] 인데 index에 5가 들어가면 안된다.
+    //board[5]=board[6]이 되는데 5까지 밖에 만들지 않아 board[6]은 들어갈수 없다.
     for (int i = index; i < (length - 1); i++) {
       boards[i] = boards[i + 1];
     }
+    //for문에 맞는다면 [2]자리에 [3]이 들어가게 되어 하나씩 땡겨지게 된다.
     --length;
     //맨 마지막 값은 없앤다 = null 적어주면 없어진다.
+    //boards[length] 는 문자열이기 때문에 0이 적힐수 없어 초기화 시키려면 null을 적어준다.
     boards[length] = null;
   }
 }
--- myapp/app/src/main/java/mystudy/myapp/MemberMenu.java
+++ myapp/app/src/main/java/mystudy/myapp/MemberMenu.java
@@ -3,7 +3,7 @@ package mystudy.myapp;
 public class MemberMenu {
 
   //static Member member = new Member();
-  static Member[] member = new Member[3];
+  static Member[] members = new Member[3];
   static int length = 0;
 
   static void printMenu() {
@@ -51,29 +51,81 @@ public class MemberMenu {
 
   static void add() {
     System.out.println("회원 등록");
+    if (length == members.length) {
+      System.out.println("회원 등록을 더이상 할수 없습니다.");
+      int oldSize = members.length;
+      int newSize = oldSize + (oldSize / 2);
+
+      Member[] arr = new Member[newSize];
+      for (int i = 0; i < oldSize; i++) {
+        arr[i] = members[i];
+      }
+      members = arr;
+    }
+
+    Member member = new Member();
     member.email = Prompt.input("이메일: ");
     member.name = Prompt.input("이름: ");
     member.password = Prompt.input("비밀번호: ");
     member.joinDate = Prompt.input("가입날짜: ");
+
+    members[length++] = member;
   }
 
   static void list() {
     System.out.println("===회원 조회===");
+    System.out.printf("%-20s\t%-10s\t%-10s\t%-10s\n", "이메일", "이름", "비밀번호", "가입날짜");
+
+    for (int i = 0; i < length; i++) {
+      Member member = members[i];
+      System.out.printf("%-20s\t%-10s\t%-10s\t%-10s\n", member.email, member.name, member.password,
+          member.joinDate);
+    }
+  }
+
+  static void view() {
+    System.out.println("회원 조회: ");
+    int index = Integer.parseInt(Prompt.input("번호? "));
+    if (index < 0 || index >= length) {
+      System.out.println("조회할 회원이 없습니다.");
+      return;
+    }
+
+    Member member = members[index];
     System.out.printf("이메일: %s\n", member.email);
     System.out.printf("이름: %s\n", member.name);
     System.out.printf("비밀번호: %s\n", member.password);
     System.out.printf("가입날짜: %s\n", member.joinDate);
+    System.out.println("-------------------------------");
   }
 
   static void modify() {
+    System.out.println("회원 수정: ");
+    int index = Integer.parseInt(Prompt.input("번호? "));
+    if (index < 0 || index >= length) {
+      System.out.println("수정할 회원이 없습니다.");
+      return;
+    }
 
+    Member member = members[index];
+    member.email = Prompt.input("이메일(%s): ", member.email);
+    member.name = Prompt.input("이름(%s): ", member.name);
+    member.password = Prompt.input("비밀번호(%s): ", member.password);
+    member.joinDate = Prompt.input("가입날짜(%s): ", member.joinDate);
   }
 
   static void delete() {
+    System.out.println("회원 삭제");
 
-  }
+    int index = Integer.parseInt(Prompt.input("번호? "));
+    if (index < 0 || index >= length) {
+      System.out.println("삭제할 회원이 없습니다");
+      return;
+    }
 
-  static void view() {
-
+    for (int i = index; i < (length - 1); i++) {
+      members[i] = members[i + 1];
+    }
+    members[--length] = null;
   }
 }
