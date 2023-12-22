fbc3d9ad6f70126c9f2f4601ec8b2118ae8134b2
choyonghun <43197549+choyonghun@users.noreply.github.com>
2023-12-22 11:27:44 +0900

    3213

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam01/Main.java
@@ -7,8 +7,10 @@ public class Main {
           Scanner sc = new Scanner(System.in);
           int A = sc.nextInt();
           int B = sc.nextInt();
-          int C = sc.nextInt();
+
           
-          System.out.println(A + B + C);
+          System.out.println( A + B );
+         
+          sc.close();
       }
 }
