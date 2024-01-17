6da310854e15f90bc7fdd1f295025998e82e12d8
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-01-17 17:34:34 +0900

    5234

--- java-basic/app/src/main/java/com/eomcs/net/ex02/Client0211.java
+++ java-basic/app/src/main/java/com/eomcs/net/ex02/Client0211.java
@@ -13,7 +13,7 @@ public class Client0211 {
     int count = 0;
     while (true) {
       try {
-        Socket socket = new Socket("localhost", 8888);
+        Socket socket = new Socket("192.168.0.49", 8888);
         System.out.printf("서버에 연결됨! - %d\n", ++count);
         // 서버의 대기열에 접속 순서대로 대기한다.
         // 서버에서 연결이 승인되면, 비로서 입출력을 할 수 있다.
