f1463b6d078b91cfb0dec6311e3f5d86e4eb3645
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-02-15 10:25:14 +0900

    2

--- myapp/app-server/src/main/java/bitcamp/myapp/vo/AttachedFile.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/vo/AttachedFile.java
@@ -34,4 +34,10 @@ public class AttachedFile {
     this.filePath = filePath;
     return this;
   }
+
+  public AttachedFile boardNo(int boardNo) {
+    this.boardNo = boardNo;
+    return this;
+  }
+
 }
