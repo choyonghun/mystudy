81a3e6dceadb2b25e5a73eef377b4feddfa78556
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-02-19 12:39:49 +0900

    3

---
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/member/MemberAddServlet.java
@@ -0,0 +1,65 @@
+package bitcamp.myapp.servlet.member;
+
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.mysql.MemberDaoImpl;
+import bitcamp.myapp.vo.Member;
+import bitcamp.util.DBConnectionPool;
+import bitcamp.util.TransactionManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/add")
+public class MemberAddServlet extends HttpServlet {
+
+  private TransactionManager txManager;
+  private MemberDao memberDao;
+
+  public MemberAddServlet() {
+    DBConnectionPool connectionPool = new DBConnectionPool(
+        "jdbc:mysql://localhost/studydb", "study", "Bitcamp!@#123");
+    txManager = new TransactionManager(connectionPool);
+    this.memberDao = new MemberDaoImpl(connectionPool);
+  }
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html lang='en'>");
+    out.println("<head>");
+    out.println("  <meta charset='UTF-8'>");
+    out.println("  <title>비트캠프 데브옵스 5기</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>회원</h1>");
+
+    try {
+      Member member = new Member();
+      member.setName(request.getParameter("name"));
+      member.setEmail(request.getParameter("email"));
+      member.setPassword(request.getParameter("password"));
+
+      memberDao.add(member);
+      out.println("<p>회원등록 하였습니다.</p>");
+
+    } catch (Exception e) {
+
+      out.println("<p>회원 등록 오류!</p>");
+      out.println("<pre>");
+      e.printStackTrace(out);
+      out.println("</pre>");
+    }
+
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
