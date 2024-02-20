3fce1f85ca5c834a216f2f94fa4e4aeb2df21484
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-02-20 16:21:41 +0900

    3

--- myapp/app/src/main/java/bitcamp/myapp/vo/Assignment.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Assignment.java
@@ -6,7 +6,6 @@ import java.sql.Date;
 public class Assignment implements Serializable {
 
   private static final long serialVersionUID = 100L;
-
   private int no;
   private String title;
   private String content;
