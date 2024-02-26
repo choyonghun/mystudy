baf1f6f122e6036f7f8c03a48dfe55e6adb3c99c
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-02-26 11:49:04 +0900

    2

--- servlet-app/app/src/main/java/bitcamp/myapp/servlet/assignment/AssignmentAddServlet.java
+++ servlet-app/app/src/main/java/bitcamp/myapp/servlet/assignment/AssignmentAddServlet.java
@@ -58,7 +58,7 @@ public class AssignmentAddServlet extends HttpServlet {
     out.println("</form>");
 
     request.getRequestDispatcher("/footer").include(request, response);
-    
+
     out.println("</body>");
     out.println("</html>");
   }
@@ -66,50 +66,19 @@ public class AssignmentAddServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html lang='en'>");
-    out.println("<head>");
-    out.println("  <meta charset='UTF-8'>");
-    out.println("  <title>비트캠프 데브옵스 5기</title>");
-    out.println("</head>");
-    out.println("<body>");
-
-    request.getRequestDispatcher("/header").include(request, response);
-
-    out.println("<h1>과제</h1>");
-
+    try {
       Assignment assignment = new Assignment();
       assignment.setTitle(request.getParameter("title"));
       assignment.setContent(request.getParameter("content"));
       assignment.setDeadline(Date.valueOf(request.getParameter("deadline")));
 
       assignmentDao.add(assignment);
-
       response.sendRedirect("/assignment/list");
 
-      request.getRequestDispatcher("/header").include(request, response);
-
-      out.println("<h1>오류!</h1>");
-
-      String message = (String) request.getAttribute("message");
-      if ( message != null) {
-        out.printf("<p>%s</p>\n", message);
-      }
-
-      Throwable exception = (Throwable) request.getAttribute("exception");
-      if (exception != null) {
-        out.println("<pre>");
-        exception.printStackTrace(out);
-        out.println("</pre>");
-      }
-
-    request.getRequestDispatcher("/footer").include(request, response);
-
-    out.println("</body>");
-    out.println("</html>");
+    } catch (Exception e) {
+      request.setAttribute("message", "과제 등록 오류!");
+      request.setAttribute("exception", e);
+      request.getRequestDispatcher("/error").forward(request, response);
+    }
   }
 }
