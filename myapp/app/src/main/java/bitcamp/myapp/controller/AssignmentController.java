dcbcb11dc9523710562d7148e43b43ab07af6178
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-03-06 14:28:53 +0900

    12

--- myapp/app/src/main/java/bitcamp/myapp/controller/AssignmentController.java
+++ myapp/app/src/main/java/bitcamp/myapp/controller/AssignmentController.java
@@ -2,9 +2,7 @@ package bitcamp.myapp.controller;
 
 import bitcamp.myapp.dao.AssignmentDao;
 import bitcamp.myapp.vo.Assignment;
-import java.sql.Date;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 public class AssignmentController {
 
@@ -14,60 +12,46 @@ public class AssignmentController {
     this.assignmentDao = assignmentDao;
   }
 
+  @RequestMapping("/assignment/form")
+  public String form() throws Exception {
+    return "/assignment/form.jsp";
+  }
+
   @RequestMapping("/assignment/add")
-  public String add(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    if (request.getMethod().equals("GET")) {
-      return "/assignment/form.jsp";
-    }
-
-    Assignment assignment = new Assignment();
-    assignment.setTitle(request.getParameter("title"));
-    assignment.setContent(request.getParameter("content"));
-    assignment.setDeadline(Date.valueOf(request.getParameter("deadline")));
-
+  public String add(Assignment assignment) throws Exception {
+    System.out.println(assignment);
     assignmentDao.add(assignment);
     return "redirect:list";
   }
 
   @RequestMapping("/assignment/list")
-  public String list(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    request.setAttribute("list", assignmentDao.findAll());
+  public String list(Map<String, Object> map) throws Exception {
+    map.put("list", assignmentDao.findAll());
     return "/assignment/list.jsp";
   }
 
   @RequestMapping("/assignment/view")
-  public String view(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    int no = Integer.parseInt(request.getParameter("no"));
+  public String view(@RequestParam("no") int no, Map<String, Object> map) throws Exception {
     Assignment assignment = assignmentDao.findBy(no);
     if (assignment == null) {
       throw new Exception("과제 번호가 유효하지 않습니다.");
     }
-    request.setAttribute("assignment", assignment);
+    map.put("assignment", assignment);
     return "/assignment/view.jsp";
   }
 
   @RequestMapping("/assignment/update")
-  public String update(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    int no = Integer.parseInt(request.getParameter("no"));
-
-    Assignment old = assignmentDao.findBy(no);
+  public String update(Assignment assignment) throws Exception {
+    Assignment old = assignmentDao.findBy(assignment.getNo());
     if (old == null) {
       throw new Exception("과제 번호가 유효하지 않습니다.");
     }
-
-    Assignment assignment = new Assignment();
-    assignment.setNo(old.getNo());
-    assignment.setTitle(request.getParameter("title"));
-    assignment.setContent(request.getParameter("content"));
-    assignment.setDeadline(Date.valueOf(request.getParameter("deadline")));
-
     assignmentDao.update(assignment);
     return "redirect:list";
   }
 
   @RequestMapping("/assignment/delete")
-  public String delete(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    int no = Integer.parseInt(request.getParameter("no"));
+  public String delete(@RequestParam("no") int no) throws Exception {
     if (assignmentDao.delete(no) == 0) {
       throw new Exception("과제 번호가 유효하지 않습니다.");
     }
--- myapp/app/src/main/java/bitcamp/myapp/servlet/RequestHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/RequestHandler.java
@@ -14,5 +14,4 @@ public class RequestHandler {
     this.controller = controller;
     this.handler = handler;
   }
-
 }
