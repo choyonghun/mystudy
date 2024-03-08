5726110f74f9533716a87ea7768265113aae39bc
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-03-08 14:47:05 +0900

    12

--- myapp/app/src/main/java/bitcamp/myapp/controller/BoardController.java
+++ myapp/app/src/main/java/bitcamp/myapp/controller/BoardController.java
@@ -11,26 +11,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
+@Controller
 public class BoardController {
 
   private TransactionManager txManager;
   private BoardDao boardDao;
   private AttachedFileDao attachedFileDao;
-  private String uploadDir = System.getProperty("board.upload.dir");
+  private String uploadDir = "";
 
   public BoardController(
       TransactionManager txManager,
       BoardDao boardDao,
-      AttachedFileDao attachedFileDao) {
+      AttachedFileDao attachedFileDao
+      ServletContext sc) {
     System.out.println("BoardController() 호출됨!");
     this.txManager = txManager;
     this.boardDao = boardDao;
     this.attachedFileDao = attachedFileDao;
+    this.uploadDir = sc.getRealPath("/upload/board");
   }
 
   @RequestMapping("/board/form")
