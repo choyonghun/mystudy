9e0e5a76f34f6e5a67c5c4993ccc0142a4a6f573
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-03-15 09:41:31 +0900

    123

--- mvc-app/app/src/main/java/bitcamp/config/AppDispatcherServletInitializer.java
+++ mvc-app/app/src/main/java/bitcamp/config/AppDispatcherServletInitializer.java
@@ -1,15 +1,5 @@
 package bitcamp.config;
 
-import javax.servlet.Registration.Dynamic;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.web.context.AbstractContextLoaderInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppDispatcherServletInitializer extends
