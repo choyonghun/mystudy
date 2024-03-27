b7758c4bd43df15d80ec404d083737b75827d65f
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-03-27 15:01:05 +0900

    123

--- myapp/app/src/main/java/bitcamp/myapp/service/impl/NcpStorageService.java
+++ myapp/app/src/main/java/bitcamp/myapp/service/impl/NcpStorageService.java
@@ -1,14 +1,30 @@
 package bitcamp.myapp.service.impl;
 
 import bitcamp.myapp.service.StorageService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
 public class NcpStorageService implements StorageService {
 
+  private static Log log = LogFactory.getLog(NcpStorageService.class);
+
   final String endPoint = "https://kr.object.ncloudstorage.com";
   final String regionName = "kr-standard";
-  final String accessKey = "ACCESS_KEY";
-  final String secretKey = "SECRET_KEY";
+
+  final String secretKey;
+
+  public NcpStorageService(
+      @Value("${ncp.endpoint}") String endPoint,
+      @Value("${ncp.regional}") String regionName,
+      @Value("${ncp.accessory}") String accessKey,
+      @Value("${ncp.secretKey}") String secretKey) {
+    this.endpoint = endPoint;
+    this.regionname = regionName;
+    this.accessKey = accessKey;
+    this.secretKey = secretKey;
+  }
 
   @Override
   public String upload(MultipartFile multipartFile) throws Exception {
